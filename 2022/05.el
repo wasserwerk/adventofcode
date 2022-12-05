a3b5f7c9047b737e202766059cbbda8387157ff9
mt <mtw@vitronic.com>
2022-12-05 17:16:01 +0100

    First commit

---
+++ 2022/05.el
@@ -0,0 +1,55 @@
+(defun read-data (file)
+  (with-temp-buffer
+    (insert-file-contents file)
+    (goto-char (point-min))
+    (let (stack
+          moves)
+      (save-excursion
+        (setq end (re-search-forward "^$" nil t)))
+      (while (re-search-forward "\\([A-Z]\\| [0-9] \\|    \\)" end t)
+        (setq stack (append stack (list (match-string 1)))))
+      (while (re-search-forward "\\([0-9]+\\)" nil t)
+        (setq moves (append moves (list (match-string 1)))))
+      (list stack moves))))
+
+(defun number-of-stacks (stack)
+  (string-to-number (car (last stack))))
+
+(defun prepare-stack (stack)
+  (setq number-of-stacks (number-of-stacks stack))
+  (mapcar (lambda (s)
+            (remove "    " s))
+          (apply 'mapcar* 'list
+                 (butlast (seq-partition stack number-of-stacks)))))
+
+(defun prepare-moves (moves)
+  (seq-partition (mapcar 'string-to-number moves) 3))
+
+(defun move-1 (stack count from to)
+  (dotimes (i count)
+    (push (pop (nth (1- from) stack)) (nth (1- to) stack))))
+
+(defun move-2 (stack count from to)
+  (let (load)
+    (dotimes (i count)
+      (push (pop (nth (1- from) stack)) load))
+    (dotimes (i count)
+      (push (pop load) (nth (1- to) stack)))))
+
+(defun top (stack)
+  (string-join (mapcar 'car stack)))
+
+(defun solve (fn)
+  (mapc (lambda (m) (apply fn stack m)) moves)
+  (top))
+
+(defun solve (file fn)
+  (let* ((number-of-stacks 0)
+         (data (read-data file))
+         (stack (prepare-stack (nth 0 data)))
+         (moves (prepare-moves (nth 1 data))))
+    (mapc (lambda (m) (apply fn stack m)) moves)
+    (top stack)))
+
+(solve "05.in" 'move-1)
+(solve "05.in" 'move-2)
