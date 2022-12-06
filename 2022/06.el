30dcb56195fea9651d6a2dffb5d22c6bb25767bc
mt <mtw@vitronic.com>
2022-12-06 15:10:27 +0100

    First commit

---
+++ 2022/06.el
@@ -0,0 +1,32 @@
+(defun read-data (file)
+  (with-temp-buffer
+    (insert-file-contents file)
+    (goto-char (point-min))
+    (buffer-string)))
+
+(defun repeat-p (seq)
+  (if (cdr seq)
+      (if (member (car seq) (cdr seq))
+          (cdr seq)
+        (repeat-p (cdr seq)))))
+
+(defun find-marker-position (seq len)
+  (loop with offset = 0
+        for rest on (split-string seq "" t)
+        until (= (length rest) len)
+        while (repeat-p (seq-take rest len))
+        finally (return (+ offset len))
+        do (incf offset)))
+
+(defun solve-1 (seq)
+  (find-marker-position seq 4))
+
+(defun solve-2 (seq)
+  (find-marker-position seq 14))
+
+(setq data (read-data "06.in"))
+
+(solve-1 data)
+
+(solve-2 data)
+
