d71822a274636ed40bd01bd4e566418c2510435d
mt <mtw@vitronic.com>
2022-12-14 14:28:24 +0100

    Redesigned for variable length of the rope

--- 2022/09.el
+++ 2022/09.el
@@ -1,6 +1,6 @@
 ;; 2022, Day 9: Rope Bridge
 
-(defvar rope '((0 . 0) (0 . 0)))
+(defvar rope ())
 
 (defvar tail-track ())
 
@@ -14,42 +14,83 @@
           (push (cons (read (current-buffer)) (read (current-buffer))) pairs)))
       (reverse pairs))))
 
-(defun init ()
+(defun init (length)
   (setq tail-track ())
-  (setq rope (list (cons 0 0) (cons 0 0)))
-  rope)
-
-(defun tail-too-far-p ()
-  (or (> (abs (- (caar rope)
-                 (caadr rope)))
-         1)
-      (> (abs (- (cdar rope)
-                 (cdadr rope)))
-         1)))
-
-(defun move (move)
-  (loop repeat (cdr move)
-        do
-        (let ((head (copy-list (car rope))))
-          (case (car move)
-            ('L (decf (caar rope)))
-            ('R (incf (caar rope)))
-            ('U (incf (cdar rope)))
-            ('D (decf (cdar rope))))
-          (if (tail-too-far-p)
-              (setf (cadr rope)
-                    head)))
-        (update-tail-track))
+  (setq rope (loop repeat length
+                   collect (cons 0 0)))
   rope)
 
 (defun update-tail-track ()
-  (add-to-list 'tail-track (copy-list (cadr rope))))
+  (add-to-list 'tail-track
+               (copy-list (car (last rope)))))
+
+(defun move-head (dir head)
+  (case dir
+    ('L (decf (car head)))
+    ('R (incf (car head)))
+    ('U (incf (cdr head)))
+    ('D (decf (cdr head))))
+  head)
+
+(defun move-vector (a b)
+  (cons (- (car a)
+           (car b))
+        (- (cdr a)
+           (cdr b))))
+
+(defun max-2 (n)
+  (cond ((>= n 2)
+         1)
+        ((<= n -2)
+         -1)
+        (t n)))
+
+(defun bring-it-closer (p)
+  (cons (max-2 (car p))
+        (max-2 (cdr p))))
+
+(defun too-far-p (m)
+  (or (> (abs (car m)) 1)
+      (> (abs (cdr m)) 1)))
+
+(defun reduce-move-vector (p m)
+  (let ((n (bring-it-closer m)))
+    (cons (+ (car p) (car n))
+          (+ (cdr p) (cdr n)))))
+
+(defun move-tail (a b)
+  (let ((m (move-vector a b)))
+    (if (too-far-p m)
+        (reduce-move-vector b m)
+      b)))
+
+(defun process-tail (h r)
+  (if (car r)
+      (let ((v (move-tail h (car r))))
+        (cons v
+              (process-tail v (cdr r))))))
+
+(defun move-rope (move)
+  (loop repeat (cdr move)
+        do (move-head (car move) (car rope))
+        do (setq rope (append (list (car rope))
+                              (process-tail (car rope) (cdr rope))))
+        do (update-tail-track))
+  rope)
+
+(defun solve (length moves)
+  (init length)
+  (mapcar 'move-rope moves)
+  (length tail-track))
 
 (defun solve-1 (moves)
-  (init)
-  (mapcar 'move moves)
-  (length tail-track))
+  (solve 2 moves))
+
+(defun solve-2 (moves)
+  (solve 10 moves))
 
 (setq moves (read-data "09.in"))
 
 (solve-1 moves)
+(solve-2 moves)
+
